67bf74b0eee319eda36b1ab8275e73f7da474aea
Parvejh <101707909+Parvejh@users.noreply.github.com>
2023-11-23 22:29:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,58 @@
-const hour = document.getElementById("hours")
-const minute = document.getElementById("minutes")
-const second = document.getElementById("seconds")
-const ampm = document.getElementById("ampm")
-
-const hourSelect = document.getElementById("hourSelector")
-const minuteSelect = document.getElementById("minuteSelector")
-const ampmSelect = document.getElementById("ampmSelector")
-const setAlarmButton = document.getElementById("setAlarm")
-const message = document.getElementById("alarm")
-const content = document.getElementById("set-alarm-container")
+const timeDisplay = document.querySelector("#time-d");
+const hourSelect = document.getElementById("hourSelector");
+const minuteSelect = document.getElementById("minuteSelector");
+const ampmSelect = document.getElementById("ampmSelector");
+const setAlarmButton = document.getElementById("setAlarm");
+const message = document.getElementById("alarm");
+const content = document.getElementById("set-alarm-container");
 
 let alarmTime;
 let currTime;
-let isAlarmSet = false
+let isAlarmSet = false ;
 
-let alarmTone = new Audio("./files/808alarm.mp3")
+let alarmTone = new Audio("./files/808alarm.mp3");
 
 function currentTime() {
         let date = new Date();
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
-        ampm.innerHTML = "AM"
+        let ampm = "AM";
 
-
-        console.log(`${hours}:${minutes} ${ampm.innerHTML}`)
-        if(alarmTime == `${hours}:${minutes} ${ampm.innerHTML}`){
-            // alert("Alarm is ringing")
-            alarmTone.play();
-            alarmTone.loop = true;
-        }
-        // console.log(`${hours} : ${minutes} : ${seconds}`)
-        currTime = `${hours} : ${minutes}`
-       if(hours >= 12){
+        if(hours >= 12){
             if(hours>12){
                 hours = hours - 12;
             }
-            ampm.innerHTML = "PM"
+            ampm = "PM"
         }else if(hours == 0){
-            ampm.innerHTML = "AM"
+            ampm = "AM"
         }
 
         hours = (hours < 10)?
-             "0" + hours :
+            "0" + hours :
             hours
         minutes = (minutes < 10)?
-             "0" + minutes :
+            "0" + minutes :
             minutes
         seconds = (seconds < 10)?
-             "0" + seconds :
-             seconds
-        hour.innerHTML = hours
-        minute.innerHTML = minutes
-        second.innerHTML = seconds
-        
+            "0" + seconds :
+            seconds
+
+        currTime = `${hours}:${minutes} ${ampm}`
+        dispTime = `${hours} : ${minutes} : ${seconds} ${ampm}`
+        timeDisplay.textContent = dispTime 
+
+        //If Current time matches the Alarm Time
+        if(alarmTime == currTime){
+            alarmTone.play();
+            alarmTone.loop = true;
+        }else{
+            alarmTone.pause();
+
+        }
 }
 function displayTime(){
-     setInterval(currentTime , 1000)
+    setInterval(currentTime , 1000)
 }
 
 function generateOption(){
@@ -79,8 +75,6 @@ function generateOption(){
             minuteSelect.options[i] = new Option(i, i)
         }
     }
-
-
 }
 
 function setAlarm(){
@@ -94,14 +88,31 @@ function setAlarm(){
         setAlarmButton.classList.add("set-button")
         content.classList.remove('disable')
 
+        message.style.visibility = "hidden"
         return isAlarmSet = false
     }
 
     isAlarmSet = true;
     //if the selected parameters are correct
     if(hourSelect.selectedIndex >=0 && minuteSelect.selectedIndex >=0 && ampmSelect.selectedIndex !=0 ){
-        let time = `${hourSelect.value}:${minuteSelect.value} ${ampmSelect.value}`
-        console.log(time)
+        let time
+        if(hourSelect.value<10){
+            if(minuteSelect.value<10){
+                time = `0${hourSelect.value}:0${minuteSelect.value} ${ampmSelect.value}`
+            }
+            else{
+                time = `0${hourSelect.value}:${minuteSelect.value} ${ampmSelect.value}`
+            }
+        }else{
+            if(minuteSelect.value<10){
+                time = `${hourSelect.value}:0${minuteSelect.value} ${ampmSelect.value}`
+            }
+            else{
+                time = `${hourSelect.value}:${minuteSelect.value} ${ampmSelect.value}`
+            }
+        }
+        
+        // console.log(time)
 
         content.classList.add('disable') //disable the section to add the alarm
 
@@ -110,14 +121,18 @@ function setAlarm(){
         setAlarmButton.classList.remove("set-button")
         setAlarmButton.classList.add("cancel-button")
         alarmTime = time;
+        message.innerHTML = `The Alarm is Set for ${alarmTime}`
+        message.style.visibility = "visible"
     }else{
         message.innerHTML = "Invalid Parameters."
         message.style.visibility = "visible"
     }
 }
 
+
+
 setAlarmButton.addEventListener('click', setAlarm);
 
 
-displayTime();
 generateOption();
+displayTime();
